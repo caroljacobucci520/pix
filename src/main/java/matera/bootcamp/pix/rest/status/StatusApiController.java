dadec83e10f5060860d7312a1d1aa1beb6af94f3
cjs <caroline.jacobucci@matera.com>
2021-09-28 21:50:17 -0300

    Inicio da classe ContaCorrenteService

--- src/main/java/matera/bootcamp/pix/rest/status/StatusApiController.java
+++ src/main/java/matera/bootcamp/pix/rest/status/StatusApiController.java
@@ -1,10 +1,16 @@
 package matera.bootcamp.pix.rest.status;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import matera.bootcamp.pix.service.ContaCorrenteService;
+
 @RestController
 public class StatusApiController {
+
+    @Autowired
+    private ContaCorrenteService contaCorrenteService;
 
     @GetMapping("/api/v1/status")
     public String statusCheck(){
